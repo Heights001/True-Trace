72adb775cd56cad7d0374e4861c74a9699d7a543
Daniel Okoh Yeboah <danielyboah777@gmail.com>
2025-12-14 18:11:54 -0800

    fix: case-sensitive filenames and build success

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link, } from "react-router-dom";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 
 const HomePage = () => {
