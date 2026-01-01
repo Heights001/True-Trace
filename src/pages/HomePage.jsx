6c2eb82cce2243ebcb7260be1da360888ccb9d81
Daniel Okoh Yeboah <danielyboah777@gmail.com>
2026-01-01 12:24:57 -0800

    swap import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link, } from "react-router-dom";
-import Footer from "../components/Footer.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 const HomePage = () => {
   return (
